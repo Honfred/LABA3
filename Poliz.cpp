210736f9b2a5e4175c464f4186b14c72d9ff2265
nabax <mr.themagnum@ya.ru>
2022-12-09 14:05:35 +0300

    fix bag

--- Poliz.cpp
+++ Poliz.cpp
@@ -1,4 +1,4 @@
-S
+//
 // Created by nabax on 25.11.22.
 //
 
@@ -77,4 +77,89 @@ void Poliz::MadePoliz(string str) {
         }
     }
     WorkEnd();
+}
+
+void Poliz::StringToWord(string str, string *_lex, int _count_lex) {
+
+}
+// '+-*/^() ',
+Poliz::Poliz(string tmp) {
+    size_mem = tmp.length();
+    count_mem = 0;
+    mem = new string[size_mem];
+    size_lex = size_mem;
+    StringToWord(tmp, lex, count_lex);
+    MadeTableOperation();
+    Opstack = TStack<TRecord>(20);
+    MadePoliz(tmp);
+}
+
+Poliz::Poliz(const Poliz &tmp) {
+    size_mem = tmp.size_mem;
+    count_mem = tmp.count_mem;
+    mem = new string[size_mem];
+    for (int i = 0; i < size_mem; i++)
+    {
+        mem[i] = tmp.mem[i];
+    }
+    lex = new string[size_mem];
+    Table_Operation = tmp.Table_Operation;
+    Opstack = tmp.Opstack;
+    Table_Operand = tmp.Table_Operand;
+}
+
+Poliz::~Poliz() {
+    delete[]mem;
+    delete[]lex;
+    delete Table_Operand;
+}
+
+Poliz &Poliz::operator=(Poliz tmp) {
+    if (size_mem != tmp.size_mem)
+    {
+        if (size_mem != 0)
+        {
+            delete[]mem;
+        }
+        size_mem = tmp.size_mem;
+        mem = new string[size_mem];
+    }
+    count_mem = tmp.count_mem;
+    for (int i = 0; i < size_mem; i++)
+    {
+        mem[i] = tmp.mem[i];
+    }
+    lex = new string[size_mem];
+    Table_Operation = tmp.Table_Operation;
+    Opstack = tmp.Opstack;
+    Table_Operand = tmp.Table_Operand;
+    return *this;
+}
+
+int Poliz::GetCount() {
+    return count_mem;
+}
+
+string Poliz::operator[](int k) {
+    if ((k >= 0)&&(k < count_mem))
+    {
+        return mem[k];
+    }
+    else
+    {
+        return " ";
+    }
+}
+
+string Poliz::GetPoliz() {
+    string str = "";
+    for (int i = 0; i < count_mem; i++)
+    {
+        str = str + mem[i] + " ";
+    }
+    return str;
+}
+
+Ttable *Poliz::GetTableOperand() {
+    return Table_Operand;
 }
